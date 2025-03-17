83f7554815070540cd9b8666362979e32f3a56e8
nutangelo <1071408623@qq.com>
2025-03-17 22:41:19 +0800

    organized

--- pl0.c
+++ pl0.c
@@ -9,13 +9,13 @@ void error(long n)
 {
     long i;
 
-    printf("Error=>");
-    for (i = 1; i <= cc-1; i++)
-    {
-        printf(" ");
-    }
+    //printf("Error=>");
+    //for (i = 1; i <= cc-1; i++)
+    //{
+    //    printf(" ");
+    //}
     
-    printf("|%s(%d)\n", err_msg[n], n);
+    //printf("|%s(%d)\n", err_msg[n], n);
     
     err++;
 }
@@ -1284,10 +1284,10 @@ int main()
     {
        // interpret();
     }
-    else
-    {
-        printf("errors in PL/0 program\n");
-    }
+    //else
+    //{
+    //    printf("errors in PL/0 program\n");
+    //}
     
     fclose(infile);
 
